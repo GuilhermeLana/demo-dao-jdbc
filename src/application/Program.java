018edaf72a72cbe5aa85a4db1eaf2de40f8285ab
GuiLana <guilherme.lana@paytrack.com.br>
2020-10-23 22:50:39 -0300

    implementado Department

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Department dp = new Department(1, "Dev");
+        System.out.println(dp);
+    }
+}
